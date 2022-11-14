ae0c0630377f715eafbce644e90795b4316da8bd
Abdulwahab O. Jimoh <omoniyi.success@gmail.com>
2022-11-14 12:35:26 +0100

    Updates

---
+++ precache-manifest.83ede7cae50eef777903ac9db8ee7e0b.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cb376ab1bcda8739c64",
+    "url": "/portfolio/static/css/main.81beffe7.chunk.css"
+  },
+  {
+    "revision": "8cb376ab1bcda8739c64",
+    "url": "/portfolio/static/js/main.30d2bdd1.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "c7fa4596809b3d00dfee",
+    "url": "/portfolio/static/css/2.65b28943.chunk.css"
+  },
+  {
+    "revision": "c7fa4596809b3d00dfee",
+    "url": "/portfolio/static/js/2.48a4ca87.chunk.js"
+  },
+  {
+    "revision": "df6340a570a49fd11ff13cecc3a7c953",
+    "url": "/portfolio/static/media/ayrem.df6340a5.png"
+  },
+  {
+    "revision": "610563da33a907f32bc73ba15e07bad9",
+    "url": "/portfolio/static/media/Ethco.610563da.png"
+  },
+  {
+    "revision": "1c1451bed0e443d02aba2f335bfc16c6",
+    "url": "/portfolio/static/media/Ownerfi.1c1451be.png"
+  },
+  {
+    "revision": "4b744583a99d3b1f5e168956112b12d4",
+    "url": "/portfolio/static/media/Techtank.4b744583.png"
+  },
+  {
+    "revision": "ec77b0678f82fd29440a77276779774e",
+    "url": "/portfolio/static/media/door_step.ec77b067.jpg"
+  },
+  {
+    "revision": "ec522b4db129ed3ad2a0bbb9718ac0ef",
+    "url": "/portfolio/static/media/ayremBg.ec522b4d.png"
+  },
+  {
+    "revision": "dc0eca726e5ea9038734c9d3e00fc59a",
+    "url": "/portfolio/static/media/image1.dc0eca72.png"
+  },
+  {
+    "revision": "62c3d247714301d257e82eac70d177a2",
+    "url": "/portfolio/static/media/image2.62c3d247.png"
+  },
+  {
+    "revision": "1acafffd3b9f56ee01ab310d1904bfad",
+    "url": "/portfolio/static/media/image3.1acafffd.png"
+  },
+  {
+    "revision": "11d29ff265555810e71979a3b5635e39",
+    "url": "/portfolio/static/media/image4.11d29ff2.png"
+  },
+  {
+    "revision": "7080a294e7e844c22e25c35b8357521d",
+    "url": "/portfolio/static/media/ethcoBg.7080a294.png"
+  },
+  {
+    "revision": "fe6041b11d6c4b8981ac17d691ea9e10",
+    "url": "/portfolio/static/media/image1.fe6041b1.png"
+  },
+  {
+    "revision": "19eb3921426b3724fb17b57a3d5942f9",
+    "url": "/portfolio/static/media/image2.19eb3921.png"
+  },
+  {
+    "revision": "515f405741dbdc889c106b5a4d97049f",
+    "url": "/portfolio/static/media/image3.515f4057.png"
+  },
+  {
+    "revision": "e4bf077ca546ca9ae1319007d49f9af8",
+    "url": "/portfolio/static/media/image.e4bf077c.png"
+  },
+  {
+    "revision": "e22f1d876e0f42634debb05667a29573",
+    "url": "/portfolio/static/media/image4.e22f1d87.png"
+  },
+  {
+    "revision": "9ac5920c482a5e88f150c41bc2e34e44",
+    "url": "/portfolio/static/media/TechtankBg.9ac5920c.png"
+  },
+  {
+    "revision": "1f690c9deac9a9b6e314234ee1aee4cc",
+    "url": "/portfolio/static/media/image1.1f690c9d.png"
+  },
+  {
+    "revision": "edbdd0c8313c42d7583db343d5cb113c",
+    "url": "/portfolio/static/media/image2.edbdd0c8.png"
+  },
+  {
+    "revision": "fd06584fad54a7216046a7b43d34ccd2",
+    "url": "/portfolio/static/media/image3.fd06584f.png"
+  },
+  {
+    "revision": "090ba8ff1d07c1017e6483ece27b3011",
+    "url": "/portfolio/static/media/image4.090ba8ff.png"
+  },
+  {
+    "revision": "c31ef644e5719692253907000ecb8090",
+    "url": "/portfolio/static/media/OwnerfiBg.c31ef644.png"
+  },
+  {
+    "revision": "d8fa056a940ab0853bed24aacd5877ea",
+    "url": "/portfolio/static/media/image1.d8fa056a.png"
+  },
+  {
+    "revision": "ea4cf83fddd02a2c353571bb272d5e2a",
+    "url": "/portfolio/static/media/image2.ea4cf83f.png"
+  },
+  {
+    "revision": "3298a0432c65758ed4c70a7c8eb8e994",
+    "url": "/portfolio/static/media/image3.3298a043.png"
+  },
+  {
+    "revision": "1b63546d9b7334519d17c5e2f76c73a4",
+    "url": "/portfolio/static/media/checkmark.1b63546d.svg"
+  },
+  {
+    "revision": "88e1b73148c94f8b64e0166b563fcb76",
+    "url": "/portfolio/index.html"
+  }
+];
